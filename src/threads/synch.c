d9e7deec5783793dd7d8ca9c9b1b833c1274f571
frank3215 <chengefei2003@163.com>
2024-06-23 16:28:31 +0800

    pass tests/threads/priority-sema
    FAIL tests/threads/priority-condvar

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -223,6 +223,8 @@ sema_up (struct semaphore *sema)
   }
   sema->value++;
   intr_set_level (old_level);
+
+  if (!intr_context ()) thread_yield();
 }
 
 static void sema_test_helper (void *sema_);
